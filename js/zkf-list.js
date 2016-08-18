b5d8dcaa4af059adcbe82855dc67cfc58ab4d1e8
zhangkaifengs <1064486551@qq.com>
2016-08-18 18:44:22 +0800

    wc

---
+++ js/zkf-list.js
@@ -0,0 +1,7 @@
+
+var num=$('.zkf_leftItem').length;
+
+var num2=$('.zkf_rConBotItem').length;
+$('.zkf_leftItem').eq(num-1).css({'borderBottom':'0'})
+$('.zkf_leftItem').eq(num-1).css({'borderBottom':'0'})
+$('.zkf_rConBotItem').eq(num2-1).css({'borderBottom':'0'})
